11b95a1a3606ec7e334fe34115bcd54ba95777ec
Dmitry Tarasov <dmitry.tarasov@live.com>
2018-06-29 02:50:45 +0500

    Adaptation for new PRC API

--- config.js
+++ config.js
@@ -1,30 +1,8 @@
-var api = "http://pool.bitsum.uz:8080";
-
-var api_blockexplorer = "http://api.bitsum.uz:8880";
-
-var poolHost = "exp.bitsum.uz";
-
-var irc = "";
-
-var email = "https://t.me/chegod";
-
-var cryptonatorWidget = ["{symbol}-BTC"];
-
-var easyminerDownload = "https://github.com/zone117x/cryptonote-easy-miner/releases/";
+//var api = "http://127.0.0.1:8880";
+var api = "http://api.bitsum.uz:8880";
 
 var blockchainExplorer = "?hash={id}#blockchain_block";
 
 var transactionExplorer = "?hash={id}#blockchain_transaction";
 
 var themeCss = "themes/default-theme.css";
-
-var networkStat = {
-    "bsm": [
-        ["pool.bitsum.uz", "http://pool.bitsum.uz:8080"],
-        ["sum.eth.uz [TAS-IX]", "http://sum.eth.uz:8117"],
-        ["sum.dreampool.info", "https://sum.dreampool.info/api/"],
-        ["sum.multipool.online", "http://sum.multipool.online:8120"],
-        ["sum.gonspool.com", "http://sum.gonspool.com:8118"],
-        ["npool.pw/bitsum", "http://sum.npool.pw:10070"],
-    ]
-};
